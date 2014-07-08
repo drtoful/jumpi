0f66878cc064274cc5ea70ca2a2512e00ae241f6
Tobias Heinzen <drtoful@users.noreply.github.com>
2014-07-08 22:02:21 +0200

    extended user model

--- jumpi/db.py
+++ jumpi/db.py
@@ -3,7 +3,8 @@
 import os
 
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, Sequence, String, create_engine
+from sqlalchemy import Column, Integer, Sequence, String, DateTime
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 _Base = declarative_base()
@@ -12,8 +13,11 @@ class User(_Base):
     __tablename__ = 'users'
 
     id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
-    fullname = Column(String)
-    sshkey = Column(String)
+    fullname = Column(String, nullable=False)
+    ssh_key = Column(String, nullable=False)
+    ssh_fingerprint = Column(String, nullable=False)
+    time_added = Column(DateTime(timezone="UTC"), nullable=False)
+    time_lastaccess = Column(DateTime(timezone="UTC"))
 
 _home = os.path.expanduser("~")
 _db_engine = create_engine("sqlite:///%s" % os.path.join(_home, "jumpi.db"))
