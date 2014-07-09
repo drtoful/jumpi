964d2932861e023c4c010acf22005b3c34c14bca
Tobias Heinzen <drtoful@users.noreply.github.com>
2014-07-09 21:14:00 +0200

    unlock in the agent

--- jumpi/agent/api.py
+++ jumpi/agent/api.py
@@ -3,7 +3,7 @@
 import json
 import os
 
-from flask import Flask
+from flask import Flask, request, Response
 from pyvault import PyVault
 from pyvault.backends.file import PyVaultFileBackend
 
@@ -16,7 +16,28 @@ _vault = PyVault(_backend)
 
 @app.route("/unlock", methods=['POST'])
 def unlock():
-    pass
+    resp = Response()
+
+    # don't do uneccesary unlocks
+    if not _vault.is_locked():
+        return ""
+
+    try:
+        data = request.json
+        if not _vault.exists():
+            _vault.create(data['passhprase'])
+        _vault.unlock(data['passphrase'])
+        if not _vault.is_locked():
+            resp.status_code = 200
+        else:
+            resp.status_code = 403
+    except:
+        resp.status_code = 500
+
+    return resp
+
+
+
 
 @app.route("/ping", methods=['GET'])
 def ping():
