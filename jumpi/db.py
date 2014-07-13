c8ddb00383c615f9d60b80c7b8f3db34107db406
Tobias Heinzen <drtoful@users.noreply.github.com>
2014-07-13 17:37:46 +0200

    permission model

--- jumpi/db.py
+++ jumpi/db.py
@@ -4,8 +4,8 @@ import os
 
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, Sequence, String, DateTime
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import ForeignKey, create_engine
+from sqlalchemy.orm import sessionmaker, backref, relationship
 
 _Base = declarative_base()
 
@@ -19,12 +19,28 @@ class User(_Base):
     time_added = Column(DateTime(timezone="UTC"), nullable=False)
     time_lastaccess = Column(DateTime(timezone="UTC"))
 
+    permissions = relationship("Permission",
+        order_by="Permission.id", backref="users")
+
 class Target(_Base):
     __tablename__ = 'targets'
 
     id = Column(String, primary_key=True)
     port = Column(Integer, nullable=False)
     type = Column(String, nullable=False)
+
+    permissions = relationship("Permission",
+        order_by="Permission.id", backref="targets")
+
+class Permission(_Base):
+    __tablename__ = 'permissions'
+
+    user_id = Column(Integer, ForeignKey("users.id"))
+    target_id = Column(String, ForeignKey("targets.id"))
+
+    id = Column(Integer, Sequence('target_id_seq'), primary_key=True)
+    user = relationship("User", backref=backref('users', order_by=id))
+    target = relationship("Target", backref=backref('targets', order_by=id))
 
 _home = os.path.expanduser("~")
 _db_engine = create_engine("sqlite:///%s" % os.path.join(_home, "jumpi.db"))
