432061ad48117e444d328ed3f71bd756108378b6
Tobias Heinzen <drtoful@users.noreply.github.com>
2014-08-06 23:17:04 +0200

    adding controls to terminal player and move to modal window

--- jumpi/web/static/terminal.js
+++ jumpi/web/static/terminal.js
@@ -1,8 +1,37 @@
+var _timeouts = [];
+
 function Terminal(lines, columns) {
     spaces = ""; for(i=0;i<columns;i++) { spaces += " "; }
 
     this.lines = [];
     for(i=0;i<lines;i++) { this.lines[this.lines.length] = spaces; }
+
+    // set new terminal to paused
+    this.running = false;
+
+    // prepare controls
+    this._controls = $("<div class=\"terminal-controls\"/>");
+    this._controls.append($("<p class=\"terminal-line\" style=\"height: 1px;\"/>").html(spaces));
+
+    var terminal = this;
+    var button = $("<button class=\"btn btn-xs terminal-button\" type=\"button\" />");
+    button.html("<span class=\"fa fa-play fa-fw\"></span>");
+    button.click(function() {
+        classes = "fa fa-fw";
+        if(!terminal.running) {
+            classes = classes + " fa-pause";
+            setTimeout(function() { terminal.run(); }, 500);
+        } else {
+            classes = classes + " fa-play";
+            terminal.pause();
+        }
+        button.html("<span class=\""+classes+"\"></span>");
+    });
+    this._controls.append(button);
+
+
+    //this._elapsed = $("<span class=\"terminal-elapsed\" />").html(" 00:00");
+    //this._controls.append(this._elapsed);
 }
 
 Terminal.prototype._draw = function() {
@@ -30,11 +59,13 @@ Terminal.prototype._update = function(self) {
         }
     }
 
-    setTimeout(function() {
-        self._draw();
-        self._index += 1;
-        self._update(self);
-    }, data.delay / 1000);
+    if(self.running) {
+        _timeouts.push(setTimeout(function() {
+            self._draw();
+            self._index += 1;
+            self._update(self);
+        }, data.delay / 1000));
+    }
 }
 
 Terminal.prototype.load = function(data) {
@@ -44,9 +75,24 @@ Terminal.prototype.load = function(data) {
 
 Terminal.prototype.attach = function(selector) {
     this.terminal = $(selector);
+
+    // cleanup old mess
+    $(".terminal-controls").remove(); // remove old terminal controls?
+    for(i=0;i<_timeouts.length;i++) {
+        clearTimeout(_timeouts[i]); // clear old timeouts
+    }
+    _timeouts = [];
+
+    // add new controls and draw empty frame
+    this.terminal.after(this._controls);
     this._draw();
 }
 
 Terminal.prototype.run = function() {
+    this.running = true;
     this._update(this);
+}
+
+Terminal.prototype.pause = function() {
+    this.running = false;
 }
