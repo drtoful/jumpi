70e77951fc3cd7bec63b9432ef0ff1cf8d8d6b67
Tobias Heinzen <drtoful@users.noreply.github.com>
2016-07-16 23:19:56 +0200

    adding auth api endpoints

--- jumpi/api.go
+++ jumpi/api.go
@@ -2,12 +2,15 @@ package jumpi
 
 import (
 	"crypto/rand"
+	"encoding/json"
 	"errors"
 	"io"
 	"log"
 	"net"
 	"net/http"
 	"os"
+	"reflect"
+	"regexp"
 	"strings"
 	"time"
 
@@ -20,9 +23,16 @@ import (
 var (
 	signingMethod = jwt.SigningMethodHS256
 	signingKey    = []byte{}
-	store         *Store
+	globalStore   *Store
 
-	ErrInvalidToken = errors.New("Invalid Authentication Token")
+	ErrInvalidToken   = errors.New("Invalid Authentication Token")
+	ErrInvalidObject  = errors.New("invalid object")
+	ErrInvalidRequest = errors.New("unrecognized request")
+
+	InternalServerError   = ErrorResponse{Status: http.StatusInternalServerError, Code: "err_internal", Description: "An internal server error has occured!"}
+	UnprocessableEntity   = ErrorResponse{Status: 422, Code: "err_unprocessable", Description: "Unable to process given entity!"}
+	AuthorizationRequired = ErrorResponse{Status: http.StatusUnauthorized, Code: "err_authorization_required", Description: "Please provide a valid Authorization Token to access this resource!"}
+	BadRequest            = ErrorResponse{Status: http.StatusBadRequest, Code: "err_bad_request"}
 )
 
 func utcnow() time.Time {
@@ -159,6 +169,36 @@ func (l *logger) ServeHTTP(w http.ResponseWriter, r *http.Request, next http.Han
 	context.Clear(r)
 }
 
+type Response struct {
+	Status  int         `json:"status"`
+	Content interface{} `json:"response"`
+}
+
+type ErrorResponse struct {
+	Status      int    `json:"status"`
+	Code        string `json:"error"`
+	Description string `json:"description"`
+}
+
+func (r ErrorResponse) Write(w http.ResponseWriter) {
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(r.Status)
+
+	if err := json.NewEncoder(w).Encode(r); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func (r Response) Write(w http.ResponseWriter) {
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(r.Status)
+
+	if err := json.NewEncoder(w).Encode(r); err != nil {
+		InternalServerError.Write(w)
+		log.Printf("unable to write answer: %s\n", err.Error())
+	}
+}
+
 func StackMiddleware(handler http.HandlerFunc, mid ...func(http.Handler) http.HandlerFunc) http.HandlerFunc {
 	for _, m := range mid {
 		handler = m(handler)
@@ -179,7 +219,7 @@ func ContextMiddleware(handler http.Handler) http.HandlerFunc {
 					continue
 				}
 
-				session := &session{store: store}
+				session := &session{store: globalStore}
 				if ok, token := session.Validate(splits[1]); ok {
 					username, _ := token.Claims["usr"].(string)
 					context.Set(r, "user", username)
@@ -194,14 +234,144 @@ func ContextMiddleware(handler http.Handler) http.HandlerFunc {
 	}
 }
 
+func CheckRequestObject(r *http.Request, definition interface{}) error {
+	if r == nil {
+		return ErrInvalidRequest
+	}
+
+	// definition needs to be a pointer to a struct
+	st := reflect.TypeOf(definition)
+	if st.Kind() != reflect.Ptr {
+		return ErrInvalidObject
+	}
+	st = st.Elem()
+	if st.Kind() != reflect.Struct {
+		return ErrInvalidObject
+	}
+
+	// parse json object from http request
+	if err := json.NewDecoder(r.Body).Decode(definition); err != nil {
+		return err
+	}
+
+	// iterate over all fields and check if the format is correct, if nothing is
+	// specified then we just accept the json decoding
+	for i := 0; i < st.NumField(); i += 1 {
+		field := st.Field(i)
+		format := field.Tag.Get("format")
+		json_name := field.Tag.Get("json")
+
+		if len(json_name) == 0 {
+			json_name = field.Name
+		}
+
+		if len(format) > 0 && field.Type.Kind() == reflect.String {
+			val := reflect.ValueOf(definition).Elem().Field(i).Interface()
+			value, ok := val.(string)
+			if !ok {
+				return errors.New("invalid field \"" + json_name + "\"")
+			}
+
+			if matched, err := regexp.MatchString(format, value); err != nil || !matched {
+				return errors.New("invalid field \"" + json_name + "\"")
+			}
+		}
+	}
+
+	return nil
+}
+
+func LoginRequired(handler http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if u := context.Get(r, "user"); u != nil {
+			handler.ServeHTTP(w, r)
+		} else {
+			AuthorizationRequired.Write(w)
+		}
+	}
+}
+
+func authLogin(w http.ResponseWriter, r *http.Request) {
+	type _json struct {
+		Username string `json:"username" format:"[a-z][a-z0-9\_\-]{2,}"`
+		Password string `json:"password" format:".+"`
+	}
+	var cred _json
+
+	if err := CheckRequestObject(r, &cred); err != nil {
+		BadRequest.Description = err.Error()
+		BadRequest.Write(w)
+		return
+	}
+
+	// check username and password
+	//TODO
+	if false {
+		AuthLoginFailed := ErrorResponse{Status: http.StatusForbidden, Code: "err_login_failed"}
+		//AuthLoginFailed.Description = err.Error()
+		AuthLoginFailed.Write(w)
+		return
+	}
+
+	session := &session{user: cred.Username, store: globalStore}
+	AuthLoginSuccessful := Response{Status: http.StatusOK}
+	AuthLoginSuccessful.Content, _ = session.Login()
+	AuthLoginSuccessful.Write(w)
+}
+
+func authLogout(w http.ResponseWriter, r *http.Request) {
+	if username, ok := context.Get(r, "user").(string); ok {
+		session := &session{store: globalStore, user: username}
+		if err := session.Logout(); err != nil {
+			AuthLogoutFailed := ErrorResponse{Status: http.StatusForbidden, Code: "err_logout_failed"}
+			AuthLogoutFailed.Description = err.Error()
+			AuthLogoutFailed.Write(w)
+			return
+		}
+	}
+
+	AuthLogoutSuccessful := Response{Status: http.StatusOK}
+	AuthLogoutSuccessful.Write(w)
+}
+
+func authValidate(w http.ResponseWriter, r *http.Request) {
+	// get authorization header and check validity of token
+	if tokens, ok := r.Header["Authorization"]; ok {
+		for _, t := range tokens {
+			// separate from first space, we want a Bearer token
+			splits := strings.Split(t, " ")
+			if len(splits) < 2 && splits[0] != "Bearer" {
+				continue
+			}
+
+			session := &session{store: globalStore}
+			if ok, _ := session.Validate(splits[1]); ok {
+				AuthTokenValid := Response{Status: http.StatusOK, Content: "Authorization Token Valid"}
+				AuthTokenValid.Write(w)
+				return
+			}
+		}
+	}
+
+	AuthTokenInvalid := Response{Status: http.StatusForbidden, Content: "Authorization Token Invalid"}
+	AuthTokenInvalid.Write(w)
+}
+
 func StartAPIServer(root string, store *Store) {
-	store = store
+	globalStore = store
 	go func() {
 		router := mux.NewRouter()
 		router.KeepContext = true // we clean context ourselves in logger
 
+		if strings.HasSuffix(root, "/") {
+			root = root[:len(root)-1]
+		}
 		api := router.PathPrefix(root + "/api/").Subrouter()
 		api = api.StrictSlash(true)
+
+		api.Path("/auth/login").Methods("POST").HandlerFunc(authLogin)
+		api.Path("/auth/logout").Methods("GET").HandlerFunc(StackMiddleware(authLogout, LoginRequired))
+		api.Path("/auth/validate").Methods("GET").HandlerFunc(authValidate)
 
 		logger := &logger{log.New(os.Stdout, "", 0)}
 		n := negroni.New(negroni.NewRecovery(), logger)
